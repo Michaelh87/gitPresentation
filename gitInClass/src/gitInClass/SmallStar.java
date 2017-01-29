d884ca1b139e8fb4cdbd4bdd26ac66daa50241ac
Michael <mholtmann@ggc.edu>
2017-01-29 10:48:04 -0500

    added small star

---
+++ gitInClass/src/gitInClass/SmallStar.java
@@ -0,0 +1,11 @@
+package gitInClass;
+
+public class SmallStar extends Stars {
+	String whatAmI = "white dwarf";
+
+	@Override
+	public String toString() {
+		return "smallStar [whatAmI=" + whatAmI + "]";
+	}
+
+}

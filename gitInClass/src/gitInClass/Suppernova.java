de8e099242fec03535d3a61c397e412d32543bba
Michael <mholtmann@ggc.edu>
2017-01-29 11:12:03 -0500

    added

---
+++ gitInClass/src/gitInClass/Suppernova.java
@@ -0,0 +1,11 @@
+package gitInClass;
+
+public class Suppernova extends Stars {
+	String whatAmI = "Expoding star";
+
+	@Override
+	public String toString() {
+		return "Suppernova [whatAmI=" + whatAmI + "]";
+	}
+
+}
